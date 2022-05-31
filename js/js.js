d8e9708c80e7975ebd3844e4206e30ad14387c7c
Alex Menov <103155422+AlexMenov@users.noreply.github.com>
2022-05-31 20:06:08 +0700

    загружает файлы сайта

---
+++ js/js.js
@@ -0,0 +1,18 @@
+const button = document.querySelector(".nav-button img");
+const nav = document.querySelector(".nav");
+let content = document.querySelector(".footer-title h2 span");
+button.addEventListener("click", () => {
+  nav.classList.toggle("open")
+    ? (button.src = "img/NAV CLOSE.svg")
+    : (button.src = "img/NAV.svg");
+});
+document
+  .querySelector(".footer-button button")
+  .addEventListener("click", () => {
+    document.querySelectorAll(`.footer-social img`).forEach((element) => {
+      element.classList.add("animate");
+      content.textContent = "Привет, Чел! Как дела?";
+      content.classList.add("contentAnimation");
+    });
+  });
+
